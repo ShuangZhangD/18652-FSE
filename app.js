1464868407b161715de4568dd3ebfec47a9a74b8
ShuangZhangD <zhangshuanghust@gmail.com>
2017-01-22 23:03:37 -0800

    final

--- app.js
+++ app.js
@@ -5,14 +5,25 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index');
+var chat = require(('./routes/index'));
 
 var app = express();
 
+// var app = require('../app');
+var debug = require('debug')('ChatNew:server');
+var http = require('http');
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+var server = http.createServer(app);
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/views/chat.html');
+});
+/*app.get('/chat',function(req,res){
+  res.sendFile(__dirname+'/views/chat.html');
+});*/
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +33,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', routes);
+app.use('/chat', chat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,15 +43,158 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./chat.db');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+/**
+ * initialize database
+ */
+db.run("CREATE TABLE IF NOT EXISTS users (userName TEXT, joinTime TEXT)");
+db.run("CREATE TABLE IF NOT EXISTS messages (userName TEXT, time TEXT, content TEXT)");
+
+var insertUser = db.prepare("insert into users Values(?,?)");
+var insertMessage = db.prepare("insert into messages Values(?,?,?)");
+/**
+ * add socket.io
+ */
+var io = require("socket.io")(server);
+
+io.on('connection', function(socket) {
+  socket.on('chat message', function(data) {
+    console.log(data.msg);
+    var time = new Date().toLocaleString();
+    insertMessage.run(data.userName, time, data.msg);
+    io.emit('broadcast messages', data);
+  });
+
+  socket.on('new user', function (userName) {
+    db.all("select * from users where userName=\"" + userName + "\"", function (err, row) {
+      if(row.length == 0) {
+        var time = new Date().toLocaleString();
+        insertUser.run(userName, time);
+        insertMessage.run(userName, time, userName + userName + " entered the chatroom");
+        console.log("new user");
+      } else {
+        console.log(row[0].joinTime);
+        db.all("select * from messages where time >\'" + row[0].joinTime + "\'", function(err, row){
+
+          //console.log(row[0].joinTime);
+          sendHistoryData(row, userName);
+        });
+        console.log("old user");
+      }
+    });
+    io.emit('new user', userName);
+  });
+
+  function sendHistoryData(messages, userName) {
+    console.log(messages.length);
+    io.emit('load old message', {messages : messages, userName : userName});
+  }
+
+  socket.on('leave', function(userName){
+    io.emit('leave', userName);
+    console.log(userName + " left the group");
+    var time = new Date().toLocaleString();
+    insertMessage.run(userName, time, userName + " left the group");
+  });
+
+});
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(app.get('port'));
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 module.exports = app;
